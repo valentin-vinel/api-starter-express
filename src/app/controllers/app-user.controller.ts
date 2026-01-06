c9f5813834a711b20100ff85a43e05a59fb96b0b
valentin-vinel <valentinvnl84@gmail.com>
2026-01-06 08:58:11 +0100

    feat(controller): add getOneUser endpoint

--- src/app/controllers/app-user.controller.ts
+++ src/app/controllers/app-user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { AppUser } from "../models/app-user.model.js";
+import { idSchema } from "../schemas/id.schema.js";
 
 // Endpoint: Get all app-users
 export const listUsers = async(req: Request, res: Response) => {
@@ -10,5 +11,23 @@ export const listUsers = async(req: Request, res: Response) => {
     } catch (error) {
         console.error("Error fetching users data : ", error)
         res.status(500).json({ error: "Failed to fetch users data."})
+    }
+}
+
+// Endpoint: Get one app-user
+export const getOneUser = async(req: Request, res: Response) => {
+    try {
+        const { id } = idSchema.parse(req.params);
+
+        const user = await AppUser.findByPk(id);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        };
+
+        res.json(user)
+    } catch (error) {
+        console.error("Error fetching user data : ", error)
+        res.status(500).json({ error: "Failed to fetch user data."})
     }
 }
