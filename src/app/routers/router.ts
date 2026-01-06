e451b35de8db2a713b1b0ea91dbf96804b836daa
valentin-vinel <valentinvnl84@gmail.com>
2026-01-06 10:49:54 +0100

    feat(router): add /ressources router

--- src/app/routers/router.ts
+++ src/app/routers/router.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import * as appUserController from "../controllers/app-user.controller.js"
+import * as ressourceController from "../controllers/ressource.controller.js"
 
 export const router = Router();
 
 router.get('/users', appUserController.listUsers);
 router.get('/users/:id', appUserController.getOneUser);
+
+router.get('/ressources', ressourceController.listRessources)
